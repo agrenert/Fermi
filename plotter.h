03763b15e78796e96d24087d24cb0a7033052a4f
Austin <51969622+agrenert@users.noreply.github.com>
2019-07-09 12:53:30 -0500

    Create plotter.h
    includes and functions for use in plotter files

---
+++ plotter.h
@@ -0,0 +1,27 @@
+#ifndef PLOTTER_H
+#define PLOTTER_H
+
+#include "TLegend.h"
+#include "TChain.h"
+#include "TROOT.h"
+#include "TCanvas.h"
+#include "TMath.h"
+#include "TTree.h"
+#include "TH2.h"
+#include "TLine.h"
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <string>
+
+// file for use in checking if files opened correctly
+void checkFile(TFile* file)
+{
+  if (file == 0)
+    {
+      std::cout << "Error: Unable to Open File..." << endl;
+      return;
+    }
+}
+
+#endif
